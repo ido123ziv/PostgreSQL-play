8ff78cfa3f6b4ab42f4233172f824076dc536ab4
idosh <idoshziv@gmail.com>
2021-11-08 23:16:28 +0200

    added insertion script

---
+++ instert_data.sql
@@ -0,0 +1,42 @@
+INSERT INTO "MMN11".team VALUES 
+(1,'a team','Red'),
+(2,'dream team','Blue'),
+(3,'Rebels','Yellow'),
+(4,'Warrios','green');
+
+
+INSERT INTO "MMN11".player VALUES 
+(99,'james smith' ,'road 1',123456789 ,1,4),
+(88,'daniel miller' ,'road 2',234567890 ,3,2),
+(77,'kevin Garcia' ,'road 3',345678901 ,4,3),
+(66,'emma scoll' ,'road 4',456789012 ,5,1),
+(44,'susan lee' ,'road 6',678901234 ,3,4),
+(33,'amy king' ,'road 7',890123456 ,2,1);
+
+INSERT INTO "MMN11".coach VALUES
+(23, 'tyler cooper', 'road 12', 987654321, 0, 4),
+(45, 'kelly morris', 'Road 13', 876543219, 1, 1),
+(67, 'adam bell', 'road 14', 765432198, 1, 2),
+(89, 'anne long', 'road 15', 543421876, 0, 3);
+
+INSERT INTO "MMN11".game VALUES
+(DATE '24.10.21', 1, 2),
+(DATE '25.10.21', 1, 3),
+(DATE '26.10.21', 2, 1),
+(DATE '27.10.21', 2, 4),
+(DATE '28.10.21', 3, 2),
+(DATE '31.10.21', 3, 4),
+(DATE '1.11.21', 4, 1),
+(DATE '2.11.21', 4, 2);
+
+INSERT INTO "MMN11".points VALUES
+(88, DATE '24.10.21', 1, 16),
+(88, DATE '26.10.21', 2, 20),
+(99, DATE '27.10.21', 2, 13),
+(44, DATE '27.10.21', 2, 20),
+(44, DATE '31.10.21', 3, 25),
+(44, DATE '1.11.21', 4, 12),
+(99, DATE '2.11.21', 4, 7),
+(44, DATE '2.11.21', 4, 34),
+(88, DATE '2.11.21', 4, 11);
+
