86ea7aa959debe5e30495f48d21bf04b54d8057e
idosh <idoshziv@gmail.com>
2021-11-12 18:02:20 +0200

    using one column

--- function.sql
+++ function.sql
@@ -3,16 +3,16 @@ SET search_path TO "MMN11", public;
 CREATE OR REPLACE FUNCTION trigf1() RETURNS TRIGGER AS $$
 DECLARE x INT;
 BEGIN
-	SELECT tid FROM player INTO x 
-	WHERE NEW.pid = player.pid;
-	IF (NEW.gdate,NEW.htid IN (SELECT gdate,htid FROM game WHERE gdate = NEW.gdate AND htid = NEW.htid)) 
+	SELECT tid FROM "MMN11".player AS P INTO x 
+	WHERE NEW.pid = P.pid;
+	IF (NEW.gdate IN (SELECT gdate FROM "MMN11".game WHERE gdate = NEW.gdate AND htid = NEW.htid)) 
 	THEN
-		IF (x IN (SELECT htid FROM game WHERE x = htid))
+		IF (x IN (SELECT htid FROM "MMN11".game WHERE x = htid))
 			THEN
-			UPDATE game SET hscore = hscore + NEW.pscore
+			UPDATE "MMN11".game SET hscore = hscore + NEW.pscore
 			WHERE htid = NEW.htid;
 		ELSE 
-			UPDATE game SET vscore = vscore + NEW.pscore
+			UPDATE "MMN11".game SET vscore = vscore + NEW.pscore
 			WHERE vtid = NEW.vtid;
 		RETURN NEW;
 		END IF;
